efd3191cb45e79a207adfe7dacb868bf6c19b31f
Hyan Hsing <hyanhsing0101@outlook.com>
2026-03-25 21:30:35 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"web_app/dao/mysql"
+	"web_app/dao/redis"
+	"web_app/logger"
+	"web_app/routes"
+	"web_app/settings"
+
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+func main() {
+	var configPath string
+	flag.StringVar(&configPath, "c", "conf/config.yaml", "配置文件路径")
+	flag.Parse()
+	if err := settings.Init(configPath); err != nil {
+		fmt.Printf("init settings failed, err:%v\n", err)
+		return
+	}
+
+	if err := logger.Init(settings.Conf.LogConfig); err != nil {
+		fmt.Printf("init logger failed, err:%v\n", err)
+		return
+	}
+	defer zap.L().Sync()
+	zap.L().Debug("init logger success")
+
+	if err := mysql.Init(settings.Conf.MySQLConfig); err != nil {
+		fmt.Printf("init mysql failed, err:%v\n", err)
+		return
+	}
+	defer mysql.Close()
+
+	if err := redis.Init(settings.Conf.RedisConfig); err != nil {
+		fmt.Printf("init redis failed, err:%v\n", err)
+		return
+	}
+	defer redis.Close()
+
+	r := routes.Setup()
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", viper.GetInt("app.port")),
+		Handler: r,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	zap.L().Info("Shutdown Server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		zap.L().Fatal("Sever Shutdown", zap.Error(err))
+	}
+	zap.L().Info("Server exiting")
+
+}
---
+++ settings/settings.go
@@ -0,0 +1,69 @@
+package settings
+
+import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+var Conf = new(AppConfig)
+
+type AppConfig struct {
+	Name         string `mapstructure:"name"`
+	Mode         string `mapstructure:"mode"`
+	Version      string `mapstructure:"version"`
+	Port         int    `mapstructure:"port"`
+	*LogConfig   `mapstructure:"log"`
+	*MySQLConfig `mapstructure:"mysql"`
+	*RedisConfig `mapstructure:"redis"`
+}
+
+type LogConfig struct {
+	Level      string `mapstructure:"level"`
+	FileName   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxAge     int    `mapstructure:"max_age"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+
+type MySQLConfig struct {
+	Host         string `mapstructure:"host"`
+	User         string `mapstructure:"user"`
+	Password     string `mapstructure:"password"`
+	DbName       string `mapstructure:"db_name"`
+	Port         int    `mapstructure:"port"`
+	MaxOpenConns int    `mapstructure:"max_open_conns"`
+	MaxIdleConns int    `mapstructure:"max_idle_conns"`
+}
+
+type RedisConfig struct {
+	Host     string `mapstructure:"host"`
+	Password string `mapstructure:"password"`
+	Port     int    `mapstructure:"port"`
+	DB       int    `mapstructure:"db"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
+
+func Init(filePath string) (err error) {
+	//viper.SetConfigName("config")
+	//viper.AddConfigPath(".")
+	//viper.AddConfigPath("./conf")
+	viper.SetConfigFile(filePath)
+	err = viper.ReadInConfig()
+	if err != nil {
+		fmt.Printf("Error reading config file, %s\n", err)
+		return
+	}
+	if err := viper.Unmarshal(Conf); err != nil {
+		fmt.Printf("Error unmarshal config file, %s\n", err)
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		fmt.Println("Config file is changed")
+		if err := viper.Unmarshal(Conf); err != nil {
+			fmt.Printf("Error unmarshal config file, %s\n", err)
+		}
+	})
+	return
+}
